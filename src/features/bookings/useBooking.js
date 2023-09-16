61070b5f2f63d28e8fd904bca9b81247db6be543
Fikri Rakala <fikrirakala@gmail.com>
2023-09-16 16:15:25 +0800

    build the single booking page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+import { useQuery } from "@tanstack/react-query";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    data: booking,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+
+  return { booking, isLoading, error };
+}
